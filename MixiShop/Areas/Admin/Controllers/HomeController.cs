146c5dbdcad4b9c57ad2905f5afd856334837a49
DOAN QUANG VINH <vinhqd>
2020-10-26 16:05:23 +0700

    Firt Project

---
+++ MixiShop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MixiShop.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+
+        public ActionResult ManageCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ManageOrder()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        public ActionResult Customers()
+        {
+            return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+
+    }
+}
